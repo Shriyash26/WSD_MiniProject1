d7f5d8dd0fa2ad72a1673291da637d78ba472d21
shri <sm2689@njit.edu>
2018-10-05 17:33:04 -0400

    Task: added main class

--- public/index.php
+++ public/index.php
@@ -6,4 +6,16 @@
  * Time: 5:01 PM
  */
 
-echo "Welcome to Heroku Project";
+main::start("example.csv");
+
+class main{
+
+    public static function start($fileName){
+
+        $records = csv::getRecords($fileName); // get csv file records
+
+        $table = html::generateTable($records); // generate table
+
+        system::printTable($table);             // print html table
+    }
+}
