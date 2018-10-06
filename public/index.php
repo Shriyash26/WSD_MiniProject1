0e58533e0a077c78a1fe86d1b84943d7d8711e91
shri <sm2689@njit.edu>
2018-10-05 23:18:39 -0400

    Task: added function to create table headers

--- public/index.php
+++ public/index.php
@@ -130,5 +130,10 @@ class html_tableHead{
 }
 // create table header
 class create_table_Header{
+    public static function createHeader ($value){
+
+        return '<th>'. $value . '</th>';
+
+    }
 
 }
